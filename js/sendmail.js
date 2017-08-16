fc96f2b3945cee06b6f7d4f017ed6172d9d37e8c
JP Meyer <jp@jpmeyer.co.za>
2017-08-16 22:22:14 +0200

    Added cool email functionality.

--- js/sendmail.js
+++ js/sendmail.js
@@ -10,10 +10,16 @@ function sendEmail() {
 
     var url = 'http://ngesabi.co.za/php/mail.php';
 
+    $("#sendMailButton").hide();
+    $("#sendingMail").show();
     $.post(url, data)
         .done(function(data) {
+            $("#sendingMail").show();
+            data = JSON.parse(data);
+            console.log(data);
             if (data.status === 200) {
-                $("#modal1").modal('close');
+                $("#modal1").modal('hide');
+                $("#thankyoumodal").modal('show');
             } else {
                 if (data.status === 400) {
                     $.each(data.errors, function(error, value) {
@@ -22,4 +28,8 @@ function sendEmail() {
                 }
             }
         })
+        .always(function() {
+            $("#sendMailButton").show();
+            $("#sendingMail").hide();
+        });
 }
